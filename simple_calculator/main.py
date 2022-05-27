8a208ffe47b0e0cabd79d633ebcdbc5267f70236
Bahey <mohamed.bahey@gizasystems.com>
2022-05-27 17:26:26 +0200

    Automated Test for Simple Calculator
    This project is to try the test automation for python code

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,1 +1,28 @@
 # -*- coding: utf-8 -*-
+from decimal import DivisionByZero
+from functools import reduce
+
+
+class SimpleCalculator:
+    def add(self, *args):
+        result = 0
+        for num in args:
+            result = result + num
+        return result
+
+    def subtract(self, *args):
+        result = args[0]
+        for num in range(1, len(args)):
+            result = result - args[num]
+        return result
+
+    def multiply(self, *args):
+        if not all(args):
+            raise ValueError
+        return reduce(lambda x, y: x * y, args)
+
+    def divide(self, numerator, denominator):
+        try:
+            return numerator / denominator
+        except ZeroDivisionError:
+            return float("inf")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,66 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from simple_calculator.main import SimpleCalculator
+import pytest
+
+
+def test_add_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5)
+
+    assert result == 9
+
+
+def test_add_multiple_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.add(5, 6, 7, 8, 9)
+
+    assert result == 35
+
+
+def test_subtract_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.subtract(5, 4)
+
+    assert result == 1
+
+
+def test_subtract_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.subtract(100, 50, 25, 15, 5)
+
+    assert result == 5
+
+
+def test_multiply_many_numbers():
+    numbers = range(1, 10)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.multiply(*numbers)
+
+    assert result == 362880
+
+
+def test_multiply_by_zero():
+    calculator = SimpleCalculator()
+    with pytest.raises(ValueError):
+        result = calculator.multiply(10, 5, 0)
+        assert result == 0
+
+
+def test_devision():
+    calculator = SimpleCalculator()
+    result = calculator.divide(10, 2)
+    assert result == 5.0
+
+
+def test_devide_by_zero():
+    calculator = SimpleCalculator()
+
+    result = calculator.divide(10, 0)
+
+    assert result == float("inf")
